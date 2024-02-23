b1a0382a182387a325e8b39ff6bcf97df4f0d31c
SmoQ <smoqxpl@gmail.com>
2024-02-23 20:19:59 +0100

    created the basics

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for, send_file, after_this_request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    # TODO: REDNER ALL ITEMS LIST
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    # TODO: Redirect to contact page
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    # TODO: form to contact
+    return render_template('contact.html')
+
+
+@app.route('/item/<item_id>')
+def display_item(item_id):
+    print("ITEM ID IS ", item_id)
+    # TODO: get an item from db
+    return render_template('item.html')
+
+
+@app.route('/user/<user_id>')
+def user_page(user_id):
+    print("USER ID IS", user_id)
+    # TODO: User page
+    return render_template('user.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
